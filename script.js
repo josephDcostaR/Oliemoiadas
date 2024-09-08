4b67ae1639cf030b228260af524255620a23054c
Joseph da costa ribeiro <87831574+josephDcostaR@users.noreply.github.com>
2024-09-07 22:26:15 -0300

    reformulacao de script

--- script.js
+++ script.js
@@ -1,55 +1,47 @@
-function pesquisar () {
+function pesquisar() {
+  // Obtém a seção HTML onde os resultados serão exibidos
   let section = document.getElementById("resultados-pesquisa");
 
-  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-  campoPesquisa = campoPesquisa.toLowerCase();
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
+      return 
+  }
 
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  // Inicializa uma string vazia para armazenar os resultados
   let resultados = "";
-  let titulo = "";
+  let titulo = ""; 
   let descricao = "";
   let tags = "";
 
+  // Itera sobre cada dado da lista de dados
   for (let dado of dados) {
-    titulo = dado.titulo.toLowerCase()
-    descricao = dado.descricao.toLowerCase()
-    tags = dado.tags.toLowerCase()
-
-    if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-      resultados += `
-        <div class="item-resultado">
-            <h2>
-                <a href="#" target="_blank">${dado.titulo}</a>
-            </h2>
-            <p class="descricao-meta">${dado.descricao}</p>
-            <a href=${dado.link} target="_blank">Mais informações</a>
-        </div>
-    `;
-    }else if(campoPesquisa == "") {
-      section.innerHTML = "<p>Nada foi encontrado. O campo de pesquisa está vazio.</p>";
-      return
-    } 
-    
-    if (!resultados) {
-      section.innerHTML = "<p>Nada foi encontrado. Não a nada sobre isso em nossa base de dados.</p>";
-      return
-    }
-    
-    if (!campoPesquisa) {
-      section.innerHTML = "<p>Nada foi encontrado. Você não buscou pelo atleta</p>";
-      return
-    }
-
-  
-
-    
-
-    
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      // se titulo includes campoPesquisa
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+          // cria um novo elemento
+          resultados += `
+          <div class="item-resultado">
+              <h2>
+                  <a href="#" target="_blank">${dado.titulo}</a>
+              </h2>
+              <p class="descricao-meta">${dado.descricao}</p>
+              <a href=${dado.link} target="_blank">Mais informações</a>
+          </div>
+      `;
+      }
   }
 
-  section.innerHTML = resultados
+  if (!resultados) {
+      resultados = "<p>Nada foi encontrado</p>"
+  }
 
+  // Atribui os resultados gerados à seção HTML
+  section.innerHTML = resultados;
 }
-
-
-
